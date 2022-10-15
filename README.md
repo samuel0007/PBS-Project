3d693626d814c9f3aed726fbc8eef00feb5d3230
samuel0007 <russo.samuel20@gmail.com>
2022-10-15 18:34:32 +0200

    add diffusion coeff + video rendering

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # PBS-Project
 
+ffmpeg command to convert frames to video:
+
+```bash
+ffmpeg -framerate 24 -i frames/%06d.png -pix_fmt yuv420p output.mp4
+```
--- proj_test/test.py
+++ proj_test/test.py
@@ -1,33 +1,44 @@
 import taichi as ti
 import math
 from scipy.integrate import solve_ivp
-
+import numpy as np
 ti.init(arch=ti.gpu)
 
 N = 1000
 
 # Global constants
+# Hint: It's quite hard to find constants s.t. the diffusion works stabily. If the simulation turns all black, reduce support radius
 K = 20.
 mu = 50.
-support_radius = 0.045
+mass = 0.012
+support_radius = 0.03
 radius = 0.01
 rest_density = 1000
+diffusion_coeff = 1e12
 dt = 4e-2 / N
-gravity = ti.Vector([0, -9.8])
-substeps = int(1 / 60 // dt)
+gravity = ti.Vector([0, -9.8, 0])
+substeps = int(1 / 180 // dt)
+total_frames = 100
+
+result_dir = "./results"
+video_manager = ti.tools.VideoManager(output_dir=result_dir, framerate=24, automatic_build=False)
+
 
 # x, v, m, T, density, a
-X = ti.Vector.field(2, dtype=ti.f32, shape=N)
-V = ti.Vector.field(2, dtype=ti.f32, shape=N)
+X = ti.Vector.field(3, dtype=ti.f32, shape=N)
+V = ti.Vector.field(3, dtype=ti.f32, shape=N)
 M = ti.field(dtype=ti.f32, shape=N)
 T = ti.field(dtype=ti.f32, shape=N)
+max_temp = 400
+colors = ti.Vector.field(3, dtype=ti.f32, shape=N)
 density = ti.field(dtype=ti.f32, shape=N)
 pressure = ti.field(dtype=ti.f32, shape=N)
-a = ti.Vector.field(2, dtype=ti.f32, shape=N)
+a = ti.Vector.field(3, dtype=ti.f32, shape=N)
+# fill temperature with random values
 
 # SPH Kernel function
 @ti.func
-def W(r_vec, h):
+def W(r_vec: ti.types.vector(3, ti.f32), h):
     r = r_vec.norm()
     value = 0.
     if r < h:
@@ -36,9 +47,9 @@ def W(r_vec, h):
 
 # SPH Kernel gradient
 @ti.func
-def gradW(r: ti.types.vector(2, ti.f32), h):
+def gradW(r: ti.types.vector(3, ti.f32), h):
     q = r.norm() / h
-    value = ti.Vector([0, 0], ti.f32)
+    value = ti.Vector([0, 0, 0], ti.f32)
     if q < 1:
         value = -45. / (math.pi * h**6) * (h - r.norm())**2 * r.normalized()
     return value
@@ -55,17 +66,16 @@ def laplW(r, h):
 @ti.kernel
 def initialize_fluid_particles():
     for particle in X:
-        X[particle] = [ti.random()*0.5, ti.random()]
+        X[particle] = [ti.random()*0.25, ti.random(), 1]
         # half of the particles have some random velocity to the left
         # V[particle] = [0, 0]
 
-        V[particle] = [ti.random()*0.1-0.1, 0]
-
-        M[particle] = 0.012
-        T[particle] = 0
+        V[particle] = [ti.random()*0.1-0.1, 0, 0]
+        M[particle] = mass
+        T[particle] = ti.random()*max_temp
         density[particle] = rest_density
         pressure[particle] = K*(density[particle] - rest_density)
-        a[particle] = [0, 0]
+        a[particle] = [0, 0, 0]
 
 
 # function reutrning the density of each particle
@@ -80,11 +90,28 @@ def update_density(particle):
         r = X[particle] - X[other]
         density[particle] += M[other] * W(r, support_radius)
     return density[particle]
+# function returning the updated temperature attribute of each particle
+@ti.func
+def update_temperature(particle):
+    derivate_temperature = 0.
+    for other in range(N):
+        r = X[particle] - X[other]
+        r_len = r.norm()
+        derivate_temperature += M[other] * (T[other] - T[particle])/density[other] * laplW(r_len, support_radius)
+    derivate_temperature *= diffusion_coeff
+    T[particle] += derivate_temperature * dt
+# function updating color according to temperature
+@ti.func
+def update_color(particle):
+    # Map temperature to 0-1 range
+    # colors[particle] = [T_normalized[particle], 0, 0]
+    colors[particle] = [T[particle] / max_temp, 0, 0]
+
 
 # function returning the force on each particle
 @ti.func
 def force(particle: ti.i32) -> ti.types.vector(2, ti.f32):
-    force = ti.Vector([0., 0.], dt=ti.f32)
+    force = ti.Vector([0., 0., 0.], dt=ti.f32)
     for other in range(N):
         if other != particle:
             r = X[particle] - X[other]
@@ -102,24 +129,40 @@ def force(particle: ti.i32) -> ti.types.vector(2, ti.f32):
 def substep():
     # The for loop iterates over all elements of the field v
     for i in X:
-
         # Update density
         density[i] = update_density(i)
         # Update pressure for each particle
         pressure[i] = update_pressure(i)
-        V[i] += force(i) / density[i] * dt
-        # Check for boundaries
-        if X[i][0] < radius and V[i][0] < 0:
-            V[i][0] *= -0.5
-        if X[i][0] > 1 - radius and V[i][0] > 0:
-            V[i][0] *= -0.5
-        if X[i][1] < radius and V[i][1] < 0:
-            V[i][1] *= -0.5
-        if X[i][1] > 1 - radius and V[i][1] > 0:
-            V[i][1] *= -0.5
-        
-        X[i] += V[i] * dt
 
+        # diffusion of attributes
+        update_temperature(i)
+        update_color(i)
+
+        # leapfrog integration of the force
+        X[i] += V[i] * dt + 0.5 * a[i] * dt**2
+        new_acc = force(i) / density[i]
+        V[i] += 0.5 * (a[i] + new_acc) * dt
+        a[i] = new_acc
+        
+        # Check for boundaries in 3d
+        if X[i][0] < 0:
+            X[i][0] = 0
+            V[i][0] = 0
+        if X[i][0] > 0.5:
+            X[i][0] = 0.5
+            V[i][0] = 0
+        if X[i][1] < 0:
+            X[i][1] = 0
+            V[i][1] = 0
+        if X[i][1] > 0.5:
+            X[i][1] = 0.5
+            V[i][1] = 0
+        if X[i][2] < 0:
+            X[i][2] = 0
+            V[i][2] = 0
+        if X[i][2] > 0.5:
+            X[i][2] = 0.5
+            V[i][2] = 0
 
 
 window = ti.ui.Window("Taichi Fluid Particle Simulation", (1024, 1024),
@@ -133,9 +176,10 @@ camera = ti.ui.Camera()
 current_t = 0.0
 
 initialize_fluid_particles()
-
-while window.running:
-    if current_t > 40:
+camera_pos = [0.5, 0.5, 2]
+# while window.running:
+for j in range(total_frames):
+    if current_t > 100:
         # Reset
         initialize_fluid_particles()
         current_t = 0
@@ -146,7 +190,10 @@ while window.running:
     
     # update_vertices()
 
-    camera.position(0.0, 0.0, 3)
+    # Camera rotates
+    # camera_pos[0] = 3 + 3 * math.sin(current_t)
+
+    camera.position(*camera_pos)
     camera.lookat(0.0, 0.0, 0)
     scene.set_camera(camera)
 
@@ -157,8 +204,20 @@ while window.running:
     #            per_vertex_color=colors,
     #            two_sided=True)
 
-    # Draw a smaller ball to avoid visual penetration
-    scene.particles(X, radius=radius, color=(0.5, 0.42, 0.8))
+    # Draw particles with each a different color
+    # print(colors)
+    # print(T)
+    # print(T_normalized)
+    scene.particles(centers=X, radius=radius, per_vertex_color=colors)
     canvas.scene(scene)
+    video_manager.write_frame(window.get_image_buffer_as_numpy())
     window.show()
+    print(f'\rFrame {j+1}/{total_frames} is recorded', end='')
+
+print('Exporting .mp4 video...')
+# this is somehow not working on my system, probably comes from ffmpeg
+# video_manager.make_video(mp4=True)
+# print(f'MP4 video is saved to {video_manager.get_output_filename(".mp4")}')
+
+    
 
