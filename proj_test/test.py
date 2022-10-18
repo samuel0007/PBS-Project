f8ef9cf6092837e17078cff0b2b2c1cf60757b09
samuel0007 <russo.samuel20@gmail.com>
2022-10-18 12:59:20 +0200

    clean code

--- proj_test/test.py
+++ proj_test/test.py
@@ -1,41 +1,44 @@
 import taichi as ti
 import math
-from scipy.integrate import solve_ivp
 import numpy as np
 ti.init(arch=ti.gpu)
 
-N = 2000
+# Simulation constants
+EXPORT_COLOR_FIELD = False
+EXPORT_FRAMES = True
+N = 100
 resolution = 24
-
-# Global constants
-# Hint: It's quite hard to find constants s.t. the diffusion works stabily. If the simulation turns all black, reduce support radius
-K = 8.
-mu = 50.
-surface_constant = 2
-mass = 0.012
-support_radius = 0.045
-radius = 0.015
-# rest_density = 1000
-alpha = 10000
-diffusion_coeff = 7e12
 dt = 6e-4
-gravity = ti.Vector([0, -30, 0])
-substeps = int(1 / 180 // dt)
-total_frames = 1000
+substeps = int(1 / 60 // dt)
+total_frames = 100
+camera_pos = [2.5, 3.5, 3]
 
 x_bound = 0.75
 y_bound = 2
 z_bound = 0.75
 
 result_dir = "./results"
-video_manager = ti.tools.VideoManager(output_dir=result_dir, framerate=24, automatic_build=False)
 
-# x, v, m, T, density, a
+# Global Physical Constants
+# Hint: It's quite hard to find constants s.t. the diffusion works stabily. If the simulation turns all black, reduce support radius
+K = 8.
+mu = 50.
+surface_constant = 2
+buoyancy_coefficient = 0.5
+gravity = ti.Vector([0, -9.81, 0])
+boundary_damping_coeff = 0.25
+mass = 0.012
+radius = 0.015
+support_radius = 0.045
+alpha = 10000
+diffusion_coeff = 7e12
+max_temp = 100
+
+# Data orientied particles informations
 X = ti.Vector.field(3, dtype=ti.f32, shape=N)
 V = ti.Vector.field(3, dtype=ti.f32, shape=N)
 M = ti.field(dtype=ti.f32, shape=N)
 T = ti.field(dtype=ti.f32, shape=N)
-max_temp = 100
 colors = ti.Vector.field(3, dtype=ti.f32, shape=N)
 rest_density = ti.field(dtype=ti.f32, shape=N)
 density = ti.field(dtype=ti.f32, shape=N)
@@ -85,7 +88,7 @@ def initialize_fluid_particles():
 # function reutrning the density of each particle
 @ti.func
 def update_pressure(particle):
-    return K * (density[particle] - rest_density[particle])
+    pressure[particle] = K * (density[particle] - rest_density[particle])
 
 @ti.func
 def update_density(particle):
@@ -95,7 +98,6 @@ def update_density(particle):
         r_len = r.norm()
         if r_len <= support_radius:
             density[particle] += M[other] * W(r_len, support_radius)
-    return density[particle]
 
 @ti.func
 def update_color_field(particle):
@@ -132,8 +134,6 @@ def update_color(particle):
     # Map temperature to 0-1 range
     colors[particle] = [T[particle] / max_temp, 0, 0]
 
-
-
 # function returning the force on each particle
 @ti.func
 def force(particle: ti.i32) -> ti.types.vector(2, ti.f32):
@@ -147,12 +147,13 @@ def force(particle: ti.i32) -> ti.types.vector(2, ti.f32):
                 force -= M[other] * (pressure[particle] + pressure[other]) / (2.*density[other]) * gradW(r_len, support_radius)
                 # viscosity force
                 force += mu * M[other] * (V[other] - V[particle]) / density[other] * laplW(r_len, support_radius)    
-
     # add surface tension force
     color_field_gradient_norm = color_field_gradient[particle].norm()
     if color_field_gradient_norm > 1e-6:
         force -= surface_constant * color_field_laplacian[particle] * color_field_gradient[particle] / color_field_gradient_norm                
 
+    # add buoyancy force
+    force += buoyancy_coefficient * (density[particle] - rest_density[particle]) * gravity
     # add gravity
     force += gravity * density[particle]
     return force
@@ -162,9 +163,9 @@ def substep():
     # The for loop iterates over all elements of the field v
     for i in X:
         # Update density
-        density[i] = update_density(i)
+        update_density(i)
         # Update pressure for each particle
-        pressure[i] = update_pressure(i)
+        update_pressure(i)
         # Update color field
         update_color_field(i)
 
@@ -182,22 +183,22 @@ def substep():
         # Check for boundaries in 3d
         if X[i][0] < 0:
             X[i][0] = 0
-            V[i][0] *= -1
+            V[i][0] *= -boundary_damping_coeff
         if X[i][0] > x_bound:
             X[i][0] = x_bound
-            V[i][0] *= -1
+            V[i][0] *= -boundary_damping_coeff
         if X[i][1] < 0:
             X[i][1] = 0
-            V[i][1] *= -1
+            V[i][1] *= -boundary_damping_coeff
         if X[i][1] > y_bound:
             X[i][1] = y_bound
-            V[i][1] *= -1
+            V[i][1] *= -boundary_damping_coeff
         if X[i][2] < 0:
             X[i][2] = 0
-            V[i][2] *= -1
+            V[i][2] *= -boundary_damping_coeff
         if X[i][2] > z_bound:
             X[i][2] = z_bound
-            V[i][2] *= -1
+            V[i][2] *= -boundary_damping_coeff
 
 @ti.func
 def evaluate_color_field(r) -> ti.f32:
@@ -208,49 +209,10 @@ def evaluate_color_field(r) -> ti.f32:
             color_field_value += M[particle] / density[particle] * W(r_len, support_radius)
     return color_field_value
 
-
-window = ti.ui.Window("Taichi Fluid Particle Simulation", (1024, 1024),
-                      vsync=True)
-
-canvas = window.get_canvas()
-canvas.set_background_color((1, 1, 1))
-scene = ti.ui.Scene()
-camera = ti.ui.Camera()
-
-current_t = 0.0
-
-# draw bounds
-bounds = ti.Vector.field(3, ti.f32, 24)
-bounds[0] = [0, 0, 0]
-bounds[1] = [0.75, 0, 0]
-bounds[2] = [0.75, 0, 0]
-bounds[3] = [0.75, 1, 0]
-bounds[4] = [0.75, 1, 0]
-bounds[5] = [0, 1, 0]
-bounds[6] = [0, 1, 0]
-bounds[7] = [0, 0, 0]
-bounds[8] = [0, 0, 0]
-bounds[9] = [0, 0, 0.75]
-bounds[10] = [0, 0, 0.75]
-bounds[11] = [0.75, 0, 0.75]
-bounds[12] = [0.75, 0, 0.75]
-bounds[13] = [0.75, 1, 0.75]
-bounds[14] = [0.75, 1, 0.75]
-bounds[15] = [0, 1, 0.75]
-bounds[16] = [0, 1, 0.75]
-bounds[17] = [0, 1, 0]
-bounds[18] = [0, 1, 0]
-bounds[19] = [0.75, 0, 0.75]
-bounds[20] = [0.75, 0, 0.75]
-bounds[21] = [0.75, 1, 0.75]
-bounds[22] = [0.75, 1, 0.75]
-bounds[23] = [0, 1, 0.75]
-
-initialize_fluid_particles()
-camera_pos = [1, 3, 3]
-
-color_field_mesh = ti.Vector.field(3, ti.f32, shape=(10, 10, 10))
-color_field_values = ti.field(ti.f32, shape=(10, 10, 10))
+@ti.kernel
+def compute_color_field():
+    for i, j, k in color_field_values:
+        color_field_values[i, j, k] = evaluate_color_field(color_field_mesh[i, j, k])
 
 # build mesh and export it
 @ti.kernel
@@ -261,18 +223,60 @@ def build_mesh():
 def export_mesh():
     np.save("results/frames_data/color_field_mesh.npy", color_field_mesh.to_numpy())
 
-build_mesh()
-export_mesh()
-
-@ti.kernel
-def compute_color_field():
-    for i, j, k in color_field_values:
-        color_field_values[i, j, k] = evaluate_color_field(color_field_mesh[i, j, k])
-
-
 def export_color_field(frame_idx: ti.i32):
+    compute_color_field()
     np.save("results/frames_data/color_field_{:04d}.npy".format(frame_idx), color_field_values.to_numpy())
 
+
+window = ti.ui.Window("Taichi Fluid Particle Simulation", (1024, 1024),
+                      vsync=True)
+
+
+canvas = window.get_canvas()
+canvas.set_background_color((1, 1, 1))
+scene = ti.ui.Scene()
+camera = ti.ui.Camera()
+video_manager = ti.tools.VideoManager(output_dir=result_dir, framerate=24, automatic_build=False)
+
+current_t = 0.0
+
+# draw bounds
+bounds = ti.Vector.field(3, ti.f32, 24)
+bounds[0] = [0, 0, 0]
+bounds[1] = [x_bound, 0, 0]
+bounds[2] = [x_bound, 0, 0]
+bounds[3] = [x_bound, y_bound, 0]
+bounds[4] = [x_bound, y_bound, 0]
+bounds[5] = [0, y_bound, 0]
+bounds[6] = [0, y_bound, 0]
+bounds[7] = [0, 0, 0]
+
+bounds[8] = [0, 0, z_bound]
+bounds[9] = [x_bound, 0, z_bound]
+bounds[10] = [x_bound, 0, z_bound]
+bounds[11] = [x_bound, y_bound, z_bound]
+bounds[12] = [x_bound, y_bound, z_bound]
+bounds[13] = [0, y_bound, z_bound]
+bounds[14] = [0, y_bound, z_bound]
+bounds[15] = [0, 0, z_bound]
+
+bounds[16] = [0, 0, 0]
+bounds[17] = [0, 0, z_bound]
+bounds[18] = [0, y_bound, 0]
+bounds[19] = [0, y_bound, z_bound]
+bounds[20] = [x_bound, 0, 0]
+bounds[21] = [x_bound, 0, z_bound]
+bounds[22] = [x_bound, y_bound, 0]
+bounds[23] = [x_bound, y_bound, z_bound]
+
+
+initialize_fluid_particles()
+
+color_field_mesh = ti.Vector.field(3, ti.f32, shape=(10, 10, 10))
+color_field_values = ti.field(ti.f32, shape=(10, 10, 10))
+
+build_mesh()
+export_mesh()
 
 # while window.running:
 for l in range(total_frames):
@@ -285,7 +289,6 @@ for l in range(total_frames):
         substep()
         current_t += dt
     
-
     camera.position(*camera_pos)
     camera.lookat(0.0, 0.0, 0)
     scene.set_camera(camera)
@@ -294,34 +297,21 @@ for l in range(total_frames):
     scene.ambient_light((0.5, 0.5, 0.5))
     # Draw particles with each a different color
     scene.particles(centers=X, radius=radius, per_vertex_color=colors)
+    scene.lines(bounds, 1)
     # Plot color field on a grid
     # color_particle_pos = ti.Vector.field(3, ti.f32, 10*10*10)
     # color_particle_color = ti.Vector.field(3, ti.f32, 10*10*10)
 
-    # print("color field computations...")
-    compute_color_field()
-    # print("finished color field computations")
-    export_color_field(l)
-    # print("flatten...")
-    # # flatten color field values and mesh
-    # color_field_mesh_flatten = ti.Vector.field(3, ti.f32, shape=(resolution*resolution*resolution))
-    # color_field_values_flatten = ti.Vector.field(3, ti.f32, shape=(resolution*resolution*resolution))
-    # for i in range(resolution):
-    #     for j in range(resolution):
-    #         for k in range(resolution):
-    #             color_field_mesh_flatten[i*resolution*resolution + j*resolution + k] = color_field_mesh[i, j, k]
-    #             color_field_values_flatten[i*resolution*resolution + j*resolution + k] = color_field_values[i, j, k]
-    # print("end flatten...")
-    
-    # scene.particles(centers=color_field_mesh_flatten, radius=0.01, per_vertex_color=color_field_values_flatten)
-    scene.lines(bounds, 1)
+    if EXPORT_COLOR_FIELD:
+        export_color_field(l)
+
     canvas.scene(scene)
-    video_manager.write_frame(window.get_image_buffer_as_numpy())
-
+    if EXPORT_FRAMES:
+        video_manager.write_frame(window.get_image_buffer_as_numpy())
+        print(f'\rFrame {l+1}/{total_frames} is recorded')
     window.show()
-    print(f'\rFrame {l+1}/{total_frames} is recorded')
 
-print('Exporting .mp4 video...')
+# print('Exporting .mp4 video...')
 # this is somehow not working on my system, probably comes from ffmpeg
 # video_manager.make_video(mp4=True)
 # print(f'MP4 video is saved to {video_manager.get_output_filename(".mp4")}')
