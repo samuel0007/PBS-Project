a53a1831f15efe09819a73b8a3f36b3005343506
b00m37 <sebastian.brovelli@hotmail.com>
2022-12-01 14:18:54 +0100

    new functionalities in pointDatareader

--- DFSPH/src/simulator/pointDatareader.py
+++ DFSPH/src/simulator/pointDatareader.py
@@ -1,20 +1,47 @@
 import pyvista as pv
+import numpy as np
 
+# This may not be needed, since it's all vtk for now
 def getFormat(file: str):
     return file[file.find('.') + 1:]
 
-def readParticles(file):
+def readParticles(file, align_mode = 0):
+    """currently only support for vtk
+    align_mode 0(leave data as is)
+    align_mode 1(align into origin corner)
+    align_mode 2(center around origin)"""
     format = getFormat(file)
+    points = None
     if format == "vtk":
         mesh = pv.read(file)
-        return mesh.points
-    if format == "bgeo":
-        # TODO
+        points = mesh.points
+    elif format == "bgeo":
+        # TODO, may not be needed
         pass
+    
+    if align_mode == 1:
+        for coord in range(3): points[0:,coord] -= min(points[0:,coord])
+    elif align_mode == 2:
+        for coord in range(3): points[0:,coord] -= (min(points[0:,coord])+max(points[0:,coord]))/2
+
+    return points    
+
+
+def readParticles_multi(filearray, align_mode = 0):
+    """currently only support for vtk
+    align_mode 0(leave data as is)
+    align_mode 1(align into origin corner)
+    align_mode 2(center around origin)"""
+    points = []
+    for file in filearray:
+        if len(points) == 0:
+            points = readParticles(file, align_mode)
+        else:
+            points = np.append(points, readParticles(file, align_mode), axis = 0)
+    return points
 
 def main(file):
-    print(getFormat(file))
-    points = readParticles(file)
+    points = readParticles_multi([file,file])
     print("shape of array:")
     print(points.shape)
     print(points)
