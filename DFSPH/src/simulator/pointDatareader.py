30b5eefb55bf979f52b91c3816b1429fd0de23e0
b00m37 <sebastian.brovelli@hotmail.com>
2022-12-04 16:47:41 +0100

    upgraded interface for reading pointDataMetaFiles

--- DFSPH/src/simulator/pointDatareader.py
+++ DFSPH/src/simulator/pointDatareader.py
@@ -1,16 +1,54 @@
 import pyvista as pv
 import numpy as np
+import os
 
-# This may not be needed, since it's all vtk for now
 def getFormat(file: str):
+    # incredible one-liner
     return file[file.find('.') + 1:]
 
-def readParticles(file, align_mode = 0):
-    """currently only support for vtk
-    align_mode 0(leave data as is)
-    align_mode 1(align into origin corner)
-    align_mode 2(center around origin)"""
+def readParticles_multi(filearray, align_modes = [], offsetarray = []):
+    """currently only support for vtk\n
+    align_mode 0(leave data as is)\n
+    align_mode 1(align into origin corner)\n
+    align_mode 2(center around origin)\n"""
+    points = []
+    i = 0
+    for file in filearray:
+        if len(points) == 0:
+            points = readParticles(file, align_modes[i], offsetarray[i])
+        else:
+            points = np.append(points, readParticles(file, align_modes[i], offsetarray[i]), axis = 0)
+        i+=1
+    return points
+
+def readParticles(file, align_mode = 0, offset = np.array([0.0, 0.0, 0.0])):
+    """currently only support for vtk or txt (metafile)\n
+    align_mode 0(leave data as is)\n
+    align_mode 1(align into origin corner)\n
+    align_mode 2(center around origin)\n"""
     format = getFormat(file)
+    if format == "txt":
+        filenames = []
+        align_modes = []
+        offsets = []
+        with open(file) as f:
+            while True:
+                pointdata = f.readline()
+                if pointdata == "":
+                    break
+                pointdata = os.path.join("src", "pointDataFiles", pointdata)
+                filenames.append(pointdata.replace('\n',''))
+                align_modes.append(int(f.readline()))
+                offset_txt = str(f.readline().replace('\n',''))
+                offset = np.fromstring(offset_txt, dtype = float, sep = ' ')
+                offsets.append(offset)
+                f.readline()
+        # print(filenames)
+        # print(align_modes)
+        # print(offsets)
+        points = readParticles_multi(filenames, align_modes, offsets)
+        return points
+
     points = None
     if format == "vtk":
         mesh = pv.read(file)
@@ -18,30 +56,22 @@ def readParticles(file, align_mode = 0):
     elif format == "bgeo":
         # TODO, may not be needed
         pass
+
     
     if align_mode == 1:
+        # even better one-liner
         for coord in range(3): points[0:,coord] -= min(points[0:,coord])
     elif align_mode == 2:
         for coord in range(3): points[0:,coord] -= (min(points[0:,coord])+max(points[0:,coord]))/2
 
+    points[:,] += offset
     return points    
 
 
-def readParticles_multi(filearray, align_mode = 0):
-    """currently only support for vtk
-    align_mode 0(leave data as is)
-    align_mode 1(align into origin corner)
-    align_mode 2(center around origin)"""
-    points = []
-    for file in filearray:
-        if len(points) == 0:
-            points = readParticles(file, align_mode)
-        else:
-            points = np.append(points, readParticles(file, align_mode), axis = 0)
-    return points
+
 
 def main(file):
-    points = readParticles_multi([file,file])
+    points = readParticles(file, 2, [1,2,1])
     print("shape of array:")
     print(points.shape)
     print(points)
@@ -58,4 +88,4 @@ def main(file):
     print((min(z_coords),max(z_coords)))
 
 if __name__ == '__main__':
-    main(r"src\pointDataFiles\BigDragonSurface.vtk")
+    main(r"src\pointDataMetaFiles\test.txt")
